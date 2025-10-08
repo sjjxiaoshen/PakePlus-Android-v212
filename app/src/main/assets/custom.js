4b3e03aff694000bb8c3b7d232a6c0b7c296bf04
sjjxiaoshen <sjj6481335810@163.com>
2025-10-08 21:48:24 +0800

    update custom js from pakeplus

--- app/src/main/assets/custom.js
+++ app/src/main/assets/custom.js
@@ -1,26 +1,25 @@
-//console.log(
-//    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
-//    'color:orangered;font-weight:bolder'
-//)
-//
-//// very important, if you don't know what it is, don't touch it
-//// 非常重要，不懂代码不要动
-//const hookClick = (e) => {
-//    const origin = e.target.closest('a')
-//    const isBaseTargetBlank = document.querySelector(
-//        'head base[target="_blank"]'
-//    )
-//    console.log('origin', origin, isBaseTargetBlank)
-//    if (
-//        (origin && origin.href && origin.target === '_blank') ||
-//        (origin && origin.href && isBaseTargetBlank)
-//    ) {
-//        e.preventDefault()
-//        console.log('handle origin', origin)
-//        location.href = origin.href
-//    } else {
-//        console.log('not handle origin', origin)
-//    }
-//}
-//
-//document.addEventListener('click', hookClick, { capture: true })
+console.log('这是一个自定义脚本内容1')
+
+// 当页面加载完成时，在页面右上角显示一个悬浮框
+window.addEventListener('DOMContentLoaded', () => {
+    const floatingBox = document.createElement('div')
+    floatingBox.style.position = 'fixed'
+    floatingBox.style.top = '10px'
+    floatingBox.style.right = '10px'
+    floatingBox.style.width = '200px'
+    floatingBox.style.height = '200px'
+    floatingBox.style.backgroundColor = 'red'
+    floatingBox.innerText = '这是一个悬浮框'
+    floatingBox.style.color = 'white'
+    floatingBox.style.textAlign = 'center'
+    floatingBox.style.lineHeight = '200px'
+    floatingBox.style.fontSize = '20px'
+    floatingBox.style.fontWeight = 'bold'
+    floatingBox.style.borderRadius = '10px'
+    floatingBox.style.boxShadow = '0 0 10px 0 rgba(0, 0, 0, 0.5)'
+    floatingBox.style.cursor = 'pointer'
+    floatingBox.addEventListener('click', () => {
+        alert('这是一个悬浮框')
+    })
+    document.body.appendChild(floatingBox)
+})
